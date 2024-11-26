cc3ef04e6cbe9118e475b45c421fcfe165f8d6f5
toNyc2017 <olds.tom@gmail.com>
2024-11-26 17:22:09 -0500

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Welcome to My App</h1>
+      <p>This is a simple React frontend connected to a Flask backend.</p>
+    </div>
+  );
+}
+
+export default App;
